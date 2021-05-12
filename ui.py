429a0100a369ed3d2b4b9405e6624fc2f3b44be4
Daniel <danielcardenascabrera2016@gmail.com>
2021-05-12 02:07:44 -0500

    Add_dataframe_and_checkboxes_wip

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 from numerical_ode_solver import NumericalOdeSolver
 import matplotlib.pyplot as plt
+import pandas as pd
 
 st.title("Solucionador de EDOs")
 
@@ -14,18 +15,25 @@ y0 = st.number_input("Valor Inicial de y")
 exact_solution = st.text_input("Solución exacta")
 if exact_solution == '':
     exact_solution = None 
-print(exact_solution)
+
 if st.button("Resolver"):
     solver = NumericalOdeSolver(func, exact_solution=exact_solution, h=h, n=n, x0=x0, y0=y0)
     results = solver.solve()
     
     fig, ax = plt.subplots(1,1) 
-    ax.plot(results['Xn'], results['Euler'], label='Euler')
-    ax.plot(results['Xn'], results['Euler Mejorado'], label='Euler Mejorado')
-    ax.plot(results['Xn'], results['RK4'], label='Runge-Kutta')
-    if exact_solution != None and exact_solution != '':
+    if st.checkbox("Euler", value=True):
+        ax.plot(results['Xn'], results['Euler'], label='Euler')
+    
+    if st.checkbox("Euler Mejorado"):
+        ax.plot(results['Xn'], results['Euler Mejorado'], label='Euler Mejorado')
+    
+    if st.checkbox("RK4"):
+        ax.plot(results['Xn'], results['RK4'], label='Runge-Kutta')
+
+    if exact_solution != None and exact_solution != '' and st.checkbox("Solución Exacta"):
         ax.plot(results['Xn'], results['Valor Real'], label='Valor Real')
     ax.legend()
     st.pyplot(fig)
-    
-    results['RK4']
+
+    df = pd.DataFrame(results)
+    st.dataframe(df)
