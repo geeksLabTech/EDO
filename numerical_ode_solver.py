ae417cfe9216d51c0299511f4f79ec0fd37a6736
Daniel <danielcardenascabrera2016@gmail.com>
2021-05-12 01:42:16 -0500

    Fix_calculation_with_exact_solution

--- numerical_ode_solver.py
+++ numerical_ode_solver.py
@@ -3,6 +3,7 @@
 from typing import Optional
 import numpy as np
 from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application
+import math
 
 class NumericalOdeSolver:
     def __init__(self, function:str, exact_solution:Optional[str]=None, h=0.1, n:int=10, x0=0.0, y0=1.0):
@@ -30,10 +31,10 @@ class NumericalOdeSolver:
         return error
 
 
-    def eval_expr(self, x, y):
+    def eval_expr(self, f, x, y):
         return parse_expr(
-            self.f, 
-            local_dict={'x': x, 'y': y},
+            f, 
+            local_dict={'x': x, 'y': y, 'e':math.e, 'pi': math.pi},
             transformations=(standard_transformations + (implicit_multiplication_application,)))
 
 
@@ -41,7 +42,7 @@ class NumericalOdeSolver:
         x, y = self.create_arrays_with_the_initial_value()
 
         for i in np.arange(1, self.n):
-            y[i] = y[i-1] + self.eval_expr(x[i-1], y[i-1]) * self.h
+            y[i] = y[i-1] + self.eval_expr(self.f, x[i-1], y[i-1]) * self.h
             x[i] = x[i-1] + self.h
         
         return x, y
@@ -51,9 +52,9 @@ class NumericalOdeSolver:
         x, y = self.create_arrays_with_the_initial_value()
         
         for i in np.arange(1, self.n):
-            y_after = y[i-1] + self.h * self.eval_expr(x[i-1], y[i-1])
+            y_after = y[i-1] + self.h * self.eval_expr(self.f, x[i-1], y[i-1])
             x[i] = x[i-1] + self.h
-            y[i] = y[i-1] + (self.eval_expr(x[i-1], y[i-1]) + self.eval_expr(x[i], y_after)) * self.h/2
+            y[i] = y[i-1] + (self.eval_expr(self.f, x[i-1], y[i-1]) + self.eval_expr(self.f, x[i], y_after)) * self.h/2
             
         return x, y
 
@@ -64,10 +65,10 @@ class NumericalOdeSolver:
         for i in np.arange(1, self.n):
             x_before = x[i-1]
             y_before = y[i-1]
-            k1 = self.eval_expr(x_before, y_before)
-            k2 = self.eval_expr(x_before + 1/2 * self.h, y_before + 1/2 * k1 * self.h)
-            k3 = self.eval_expr(x_before + 1/2* self.h, y_before + 1/2 * k2* self.h)
-            k4 = self.eval_expr(x_before + self.h, y_before + k3 * self.h)
+            k1 = self.eval_expr(self.f, x_before, y_before)
+            k2 = self.eval_expr(self.f, x_before + 1/2 * self.h, y_before + 1/2 * k1 * self.h)
+            k3 = self.eval_expr(self.f, x_before + 1/2* self.h, y_before + 1/2 * k2* self.h)
+            k4 = self.eval_expr(self.f, x_before + self.h, y_before + k3 * self.h)
             m = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
             y[i] = y_before + m * self.h
             x[i] = x[i-1] + self.h
@@ -75,14 +76,13 @@ class NumericalOdeSolver:
         return x, y
 
 
-    def solve_with_exact_solution(self):
-        x, y = self.create_arrays_with_the_initial_value()
+    def solve_with_exact_solution(self, x):
+        y = np.zeros(self.n)
         
-        for i in np.arange(1, self.n):
-            y[i] = self.eval_expr(x[i-1], y[i-1])
-            x[i] = x[i-1]
-
-        return x, y
+        for i in np.arange(0, self.n):
+            y[i] = self.eval_expr(self.exact_solution, x[i], None)
+            
+        return y
     
     
     def solve(self):
@@ -91,11 +91,11 @@ class NumericalOdeSolver:
         s3 = self.rk4()
         solutions = {"Xn": s1[0], "Euler": s1[1], "Euler Mejorado": s2[1], "RK4": s3[1]}
         if self.exact_solution is not None:
-            s4 = self.solve_with_exact_solution()
-            solutions["Valor Real"] = s4[1]
-            solutions["Error Absoluto Euler"] = self.absolute_error(s1[1], s4[1])
-            solutions["Error Absoluto Euler Mejorado"] = self.absolute_error(s1[1], s4[1])
-            solutions["Error Absoluto RK4"] = self.absolute_error(s1[1], s4[1])
+            s4 = self.solve_with_exact_solution(s1[0])
+            solutions["Valor Real"] = s4
+            solutions["Error Absoluto Euler"] = self.absolute_error(s1[1], s4)
+            solutions["Error Absoluto Euler Mejorado"] = self.absolute_error(s1[1], s4)
+            solutions["Error Absoluto RK4"] = self.absolute_error(s1[1], s4)
 
         return solutions
     
